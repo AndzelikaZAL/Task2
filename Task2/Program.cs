95d5e3dee083b74258361d898b7d746d0f102b7a
MariusM <mariukaz@gmail.com>
2021-10-29 17:48:07 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            String skaiciusPirmas;
+           
+
+            Console.WriteLine("Enter first number:");
+            skaiciusPirmas = Console.ReadLine();
+            Console.WriteLine("Enter second number:");
+            String skaiciusAntras = Console.ReadLine();
+
+            float result = float.Parse(skaiciusPirmas) + int.Parse(skaiciusAntras);
+
+            Console.WriteLine($"Sum is: {result}"); // "result" is variable name;
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
